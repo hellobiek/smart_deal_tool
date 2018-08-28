09024ce9a16afc92310425fbd7dfb1a2a0026c78
hellobiek <hellobiek@gmail.com>
2018-08-28 12:53:07 +0800

    add new code

--- data_manager.py
+++ data_manager.py
@@ -260,7 +260,7 @@ class DataManager:
         obj_pool = Pool(100)
         df = self.stock_info_client.get()
         for _index, code_id in df.code.iteritems():
-            logger.info("all-tick index:%s, code:%s" % ((_index + 1), code_id))
+            logger.info("all tick index:%s, code:%s" % ((_index + 1), code_id))
             _obj = self.stock_objs[code_id] if code_id in self.stock_objs else CStock(self.dbinfo, code_id)
             for _date in date_only_array:
                 if self.cal_client.is_trading_day(_date):
