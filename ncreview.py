d74b9a3c008786fbe0ed97c79d5bfd45613930c4
hellobiek <hellobiek@gmail.com>
2018-09-05 23:46:10 +0800

    add new code

---
+++ ncreview.py
@@ -0,0 +1,324 @@
+#-*- coding: utf-8 -*-
+import gevent
+from gevent import monkey
+monkey.patch_all(thread=True)
+from gevent.pool import Pool
+from greenlet import greenlet
+import os
+import time
+import json
+import _pickle
+import datetime
+from datetime import datetime, date
+import traceback
+import const as ct
+import numpy as np
+import pandas as pd
+import matplotlib
+matplotlib.use('Agg')
+from matplotlib import style
+import matplotlib.pyplot as plt
+import matplotlib.lines as mlines
+import matplotlib.dates as mdates
+import matplotlib.ticker as ticker
+import matplotlib.animation as animation
+from matplotlib.font_manager import FontProperties
+from cdoc import CDoc
+from cmysql import CMySQL
+from cstock import CStock
+from cindex import CIndex
+from climit import CLimit
+from cstock_info import CStockInfo 
+from industry_info import IndustryInfo
+import ccalendar
+from common import create_redis_obj, is_trading_time, is_afternoon, delta_days
+from log import getLogger
+logger = getLogger(__name__)
+
+def get_chinese_font():
+    return FontProperties(fname='/conf/fonts/PingFang.ttc')
+
+class CReivew:
+    def __init__(self, dbinfo):
+        self.dbinfo = dbinfo
+        self.sdir = '/data/docs/blog/hellobiek.github.io/source/_posts'
+        self.doc = CDoc(self.sdir)
+        self.stock_objs = dict()
+        self.redis = create_redis_obj()
+        self.mysql_client = CMySQL(self.dbinfo)
+        self.cal_client = ccalendar.CCalendar(without_init = True)
+        self.animating = False
+        self.emotion_table = ct.EMOTION_TABLE
+        if not self.create_emotion(): raise Exception("create emotion table failed")
+
+    def create_emotion(self):
+        if self.emotion_table not in self.mysql_client.get_all_tables():
+            sql = 'create table if not exists %s(date varchar(10) not null, score float, PRIMARY KEY (date))' % self.emotion_table 
+            if not self.mysql_client.create(sql, self.emotion_table): return False
+        return True
+
+    def get_today_all_stock_data(self, _date):
+        df_byte = self.redis.get(ct.TODAY_ALL_STOCK)
+        if df_byte is None: return None
+        df = _pickle.loads(df_byte)
+        return df[df.date == _date]
+
+    def get_industry_data(self, _date):
+        df = pd.DataFrame()
+        df_info = IndustryInfo.get()
+        for _, code in df_info.code.iteritems():
+            data = CIndex(self.dbinfo, code).get_k_data(date = _date)
+            df = df.append(data)
+            df = df.reset_index(drop = True)
+        df['name'] = df_info['name']
+        df = df.sort_values(by = 'amount', ascending= False)
+        df = df.reset_index(drop = True)
+        return df
+
+    def emotion_plot(self, dir_name):
+        sql = "select * from %s" % self.emotion_table
+        df = self.mysql_client.get(sql)
+        fig = plt.figure()
+        x = df.date.tolist()
+        xn = range(len(x))
+        y = df.score.tolist()
+        plt.plot(xn, y)
+        for xi, yi in zip(xn, y):
+            plt.plot((xi,), (yi,), 'ro')
+            plt.text(xi, yi, '%s' % yi)
+        plt.scatter(xn, y, label='score', color='k', s=25, marker="o")
+        plt.xticks(xn, x)
+        plt.xlabel('时间', fontproperties = get_chinese_font())
+        plt.ylabel('分数', fontproperties = get_chinese_font())
+        plt.title('股市情绪', fontproperties = get_chinese_font())
+        fig.autofmt_xdate()
+        plt.savefig('%s/emotion.png' % dir_name, dpi=1000)
+
+    def industry_plot(self, dir_name, industry_info):
+        #colors = ['#F5DEB3', '#A0522D', '#1E90FF', '#FFE4C4', '#00FFFF', '#DAA520', '#3CB371', '#808080', '#ADFF2F', '#4B0082', '#ADD8E6']
+        colors = ['#F5DEB3', '#A0522D', '#1E90FF', '#FFE4C4', '#00FFFF', '#DAA520', '#3CB371', '#808080', '#ADFF2F', '#4B0082']
+        industry_info.amount = industry_info.amount / 10000000000
+        total_amount = industry_info.amount.sum()
+        amount_list = industry_info[0:10].amount.tolist()
+        x = date.fromtimestamp(time.time())
+        fig = plt.figure()
+        base_line = 0 
+        for i in range(len(amount_list)):
+            label_name = "%s:%s" % (industry_info.loc[i]['name'], 100 * amount_list[i] / total_amount)
+            plt.bar(x, amount_list[i], width = 0.1, color = colors[i], bottom = base_line, align = 'center', label = label_name)
+            base_line += amount_list[i]
+        plt.gca().xaxis.set_major_formatter(mdates.DateFormatter('%m/%d/%Y'))
+        plt.gca().xaxis.set_major_locator(mdates.DayLocator())
+        plt.xlabel('x轴', fontproperties = get_chinese_font())
+        plt.ylabel('y轴', fontproperties = get_chinese_font())
+        plt.title('市值分布', fontproperties = get_chinese_font())
+        fig.autofmt_xdate()
+        plt.legend(loc = 'upper right', prop = get_chinese_font())
+        plt.savefig('%s/industry.png' % dir_name, dpi=1000)
+
+    def get_limitup_data(self, date):
+        return CLimit(self.dbinfo).get_data(date)
+
+    def gen_market_emotion_score(self, stock_info, limit_info):
+        limit_up_list = limit_info[(limit_info.pchange > 0) & (limit_info.prange != 0)].reset_index(drop = True).code.tolist()
+        limit_down_list = limit_info[limit_info.pchange < 0].reset_index(drop = True).code.tolist()
+        limit_up_list.extend(limit_down_list)
+        total = 0
+        for _index, pchange in stock_info.changepercent.iteritems():
+            code = str(stock_info.loc[_index, 'code']).zfill(6)
+            if code in limit_up_list: 
+                total += 2 * pchange
+            else:
+                total += pchange
+        aver = total / len(stock_info)
+        data = {'date':["%s" % datetime.now().strftime('%Y-%m-%d')], 'score':[aver]}
+        df = pd.DataFrame.from_dict(data)
+        if not self.mysql_client.set(df, self.emotion_table):
+            raise Exception("set data to emotion failed")
+
+    def static_plot(self, dir_name, stock_info, limit_info):
+        colors = ['b', 'r', 'y', 'g', 'm']
+        limit_up_list   = limit_info[(limit_info.pchange > 0) & (limit_info.prange != 0)].reset_index(drop = True).code.tolist()
+        limit_down_list = limit_info[limit_info.pchange < 0].reset_index(drop = True).code.tolist()
+        limit_list = limit_up_list + limit_down_list
+        changepercent_list = [9, 7, 5, 3, 1, 0, -1, -3, -5, -7, -9]
+        num_list = list()
+        name_list = list()
+        num_list.append(len(limit_up_list))
+        name_list.append("涨停")
+        c_length = len(changepercent_list)
+        for _index in range(c_length):
+            pchange = changepercent_list[_index]
+            if 0 == _index:
+                num_list.append(len(stock_info[(stock_info.changepercent > pchange) & (stock_info.loc[_index, 'code'] not in limit_list)]))
+                name_list.append(">%s" % pchange)
+            elif c_length - 1 == _index:
+                num_list.append(len(stock_info[(stock_info.changepercent < pchange) & (stock_info.loc[_index, 'code'] not in limit_list)]))
+                name_list.append("<%s" % pchange)
+            else:
+                p_max_change = changepercent_list[_index - 1]
+                num_list.append(len(stock_info[(stock_info.changepercent > pchange) & (stock_info.changepercent < p_max_change)]))
+                name_list.append("%s-%s" % (pchange, p_max_change))
+        num_list.append(len(limit_down_list))
+        name_list.append("跌停")
+    
+        fig = plt.figure()
+        for i in range(len(num_list)):
+            plt.bar(i + 1, num_list[i], color = colors[i % len(colors)], width = 0.3)
+            plt.text(i + 1, 15 + num_list[i], num_list[i], ha = 'center', font_properties = get_chinese_font())
+    
+        plt.xlabel('x轴', fontproperties = get_chinese_font())
+        plt.ylabel('y轴', fontproperties = get_chinese_font())
+        plt.title('涨跌分布', fontproperties = get_chinese_font())
+        plt.xticks(range(1, len(num_list) + 1), name_list, fontproperties = get_chinese_font())
+        fig.autofmt_xdate()
+        plt.savefig('%s/static.png' % dir_name, dpi=1000)
+
+    def is_collecting_time(self):
+        now_time = datetime.now()
+        _date = now_time.strftime('%Y-%m-%d')
+        y,m,d = time.strptime(_date, "%Y-%m-%d")[0:3]
+        mor_open_hour,mor_open_minute,mor_open_second = (21,0,0)
+        mor_open_time = datetime(y,m,d,mor_open_hour,mor_open_minute,mor_open_second)
+        mor_close_hour,mor_close_minute,mor_close_second = (23,59,59)
+        mor_close_time = datetime(y,m,d,mor_close_hour,mor_close_minute,mor_close_second)
+        return mor_open_time < now_time < mor_close_time
+
+    def get_index_data(self, _date):
+        df = pd.DataFrame()
+        for code, name in ct.TDX_INDEX_DICT.items():
+            self.mysql_client.changedb(CIndex.get_dbname(code))
+            data = self.mysql_client.get("select * from day where cdate=\"%s\";" % _date)
+            data['name'] = name
+            df = df.append(data)
+        self.mysql_client.changedb()
+        return df
+
+    def update(self):
+        _date = datetime.now().strftime('%Y-%m-%d')
+        dir_name = os.path.join(self.sdir, "%s-StockReView" % _date)
+        try:
+            if not os.path.exists(dir_name):
+                logger.info("create daily info")
+                #stock analysis
+                stock_info = self.get_today_all_stock_data(_date)
+                #get volume > 0 stock list
+                stock_info = stock_info[stock_info.volume > 0]
+                stock_info = stock_info.reset_index(drop = True)
+                os.makedirs(dir_name, exist_ok = True)
+                #industry analysis
+                industry_info = self.get_industry_data(_date)
+                #index and total analysis
+                index_info = self.get_index_data(_date)
+                index_info = index_info.reset_index(drop = True)
+                #limit up and down analysis
+                limit_info = self.get_limitup_data(_date)
+                #emotion analysis
+                self.gen_market_emotion_score(stock_info, limit_info)
+                self.emotion_plot(dir_name)
+                #static analysis
+                self.static_plot(dir_name, stock_info, limit_info)
+                #gen review file
+                self.doc.generate(stock_info, industry_info, index_info)
+                #gen review animation
+                self.gen_animation()
+        except Exception as e:
+            logger.error(e)
+
+    def gen_animation(self, sfile = None):
+        style.use('fivethirtyeight')
+        Writer = animation.writers['ffmpeg']
+        writer = Writer(fps=1, metadata=dict(artist='biek'), bitrate=1800)
+        fig = plt.figure()
+        ax = fig.add_subplot(1,1,1)
+        _today = datetime.now().strftime('%Y-%m-%d')
+        cdata = self.mysql_client.get('select * from %s where cdate = "%s"' % (ct.ANIMATION_INFO, _today))
+        if cdata is None: return None
+        cdata = cdata.reset_index(drop = True)
+        ctime_list = cdata.ctime.unique()
+        name_list = cdata.name.unique()
+        ctime_list = [datetime.strptime(ctime,'%H:%M:%S') for ctime in ctime_list]
+        frame_num = len(ctime_list)
+        if 0 == frame_num: return None
+        def animate(i):
+            ax.clear()
+            ax.xaxis.set_major_formatter(mdates.DateFormatter('%H:%M:%S'))
+            ax.xaxis.set_major_locator(mdates.DayLocator())
+            ax.set_title('盯盘', fontproperties = get_chinese_font())
+            ax.set_xlabel('时间', fontproperties = get_chinese_font())
+            ax.set_ylabel('增长', fontproperties = get_chinese_font())
+            ax.set_ylim((-6, 6))
+            fig.autofmt_xdate()
+            for name in name_list:
+                pchange_list = list()
+                price_list = cdata[cdata.name == name]['price'].tolist()
+                pchange_list.append(0)
+                for _index in range(1, len(price_list)):
+                    pchange_list.append(10 * (price_list[_index] - price_list[_index - 1])/price_list[0])
+                ax.plot(ctime_list[0:i], pchange_list[0:i], label = name, linewidth = 1.5)
+                if pchange_list[i-1] > 1 or pchange_list[i-1] < -1:
+                    ax.text(ctime_list[i-1], pchange_list[i-1], name, font_properties = get_chinese_font())
+        ani = animation.FuncAnimation(fig, animate, frame_num, interval = 60000, repeat = False)
+        sfile = '/data/animation/%s_animation.mp4' % _today if sfile is None else sfile
+        ani.save(sfile, writer)
+        plt.close(fig)
+
+    def gen_stocks_trends(self, _date):
+        stock_data = self.get_today_all_stock_data(_date)
+        stock_info = CStockInfo.get()
+        stock_data = stock_data[stock_data.code.isin(stock_info.code.tolist())]
+        stock_data = stock_data.reset_index(drop = True)
+        start_date = '2018-07-05'
+        end_date   = _date
+        ndays = delta_days(start_date, end_date)
+        date_dmy_format = time.strftime("%m/%d/%Y", time.strptime(start_date, "%Y-%m-%d"))
+        data_times = pd.date_range(date_dmy_format, periods=ndays, freq='D')
+        date_only_array = np.vectorize(lambda s: s.strftime('%Y-%m-%d'))(data_times.to_pydatetime())
+        greenlets = list()
+        logger.info("read start")
+        obj_pool = Pool(400)
+        for code in stock_data.code:
+            sql = "select * from day where cdate between \"%s\" and \"%s\"" %(start_date, end_date)
+            self.mysql_client.changedb(CStock.get_dbname(code))
+            if obj_pool.full():
+                obj_pool.wait_available()
+                print("available now")
+            g = obj_pool.spawn(self.mysql_client.get, sql)
+            greenlets.append(g)
+            obj_pool.add(g)
+        obj_pool.join(timeout = 120)
+        obj_pool.kill()
+        self.mysql_client.changedb()
+        logger.info("read succeed")
+        all_df = pd.DataFrame()
+        while(len(greenlets) > 0):
+            logger.info("1:%s" % len(greenlets))
+            for i in range(len(greenlets) - 1, -1, -1):
+                if greenlets[i].ready():
+                    if greenlets[i].value is not None:
+                        all_df = all_df.append(greenlets[i].value)
+                        del greenlets[i]
+                        logger.info("2:%s" % len(greenlets))
+                    else:
+                        logger.info(greenlets[i].name)
+                        greenlets[i].start()
+                        greenlets[i].join(timeout = 10)
+        return all_df
+
+if __name__ == '__main__':
+    _date = datetime.now().strftime('%Y-%m-%d')
+    creview = CReivew(ct.DB_INFO)
+    df = creview.gen_stocks_trends('2018-09-05')
+    with open('temp.json', 'w') as f:
+        f.write(df.to_json(orient='records', lines=True))
+
+    with open('temp.json', 'r') as f:
+        data = json.load(f)
+        df = pd.read_json(data, orient='records')
+
+    print(df)
+    #for index, code in df.code.iteritems():
+    #    p_df  = df[df.code ==  code]
+    #    print(p_df)
+    #    import sys
+    #    sys.exit(0)
