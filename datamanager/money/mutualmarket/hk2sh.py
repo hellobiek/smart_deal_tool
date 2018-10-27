79a160f8d0431493eba77fd3fa2156f7f62d2788
hellobiek <hellobiek@gmail.com>
2018-10-27 12:21:38 +0800

    add new code for capatical anaysis

--- datamanager/money/mutualmarket/hk2sh.py
+++
@@ -1,8 +0,0 @@
-#coding=utf-8
-from log import getLogger
-from combination import Combination
-logger = getLogger(__name__)
-
-class HK2SH(Combination):
-    def __init__(self, code, dbinfo = ct.DB_INFO, redis_host = None):
-        Combination.__init__(self, dbinfo, code, redis_host)
