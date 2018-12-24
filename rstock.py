42d7854e6ec5c87ae17ca4aedad2b5f77eb77a80
hellobiek <hellobiek@gmail.com>
2018-12-24 11:20:34 +0800

    add new code

--- rstock.py
+++ rstock.py
@@ -111,7 +111,7 @@ class RIndexStock:
 
     def generate_all_data(self, cdate):
         good_list = list()
-        obj_pool = Pool(500)
+        obj_pool = Pool(50)
         all_df = pd.DataFrame()
         #stock_info = CStockInfo.get()
         #failed_list = stock_info.code.tolist()
