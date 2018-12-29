0c52874396beb90262401e10b9279d3740e5b443
hellobiek <hellobiek@gmail.com>
2018-12-29 11:58:22 +0800

    fix bug of get preday

--- rstock.py
+++ rstock.py
@@ -133,7 +133,7 @@ class RIndexStock:
         return all_df
 
     def update(self, end_date = datetime.now().strftime('%Y-%m-%d'), num = 19):
-        if end_date == datetime.now().strftime('%Y-%m-%d'): end_date = get_day_nday_ago(end_date, num = 1, dformat = "%Y-%m-%d")
+        #if end_date == datetime.now().strftime('%Y-%m-%d'): end_date = get_day_nday_ago(end_date, num = 1, dformat = "%Y-%m-%d")
         start_date = get_day_nday_ago(end_date, num = num, dformat = "%Y-%m-%d")
         date_array = get_dates_array(start_date, end_date)
         succeed = True
