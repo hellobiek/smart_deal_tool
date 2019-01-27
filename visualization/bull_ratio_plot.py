0e28b8c1aa5af5f9977495bf31dbd2d4cee68efb
hellobiek <hellobiek@gmail.com>
2019-01-27 17:02:51 +0800

    add new code

---
+++ visualization/bull_ratio_plot.py
@@ -0,0 +1,65 @@
+#coding=utf-8
+import os
+import sys
+from os.path import abspath, dirname
+sys.path.insert(0, dirname(dirname(abspath(__file__))))
+import const as ct
+import numpy as np
+import pandas as pd
+import datetime as dt
+from cindex import CIndex
+from rprofit import RProfit
+from rstock import RIndexStock
+import matplotlib.pyplot as plt
+from matplotlib.pylab import date2num
+from matplotlib import dates as mdates
+from matplotlib import ticker as mticker
+from mpl_finance import candlestick_ohlc
+from matplotlib.widgets import MultiCursor
+from matplotlib.dates import DateFormatter
+class CBullRation():
+    def __init__(self):
+        self.rp  = RProfit(dbinfo = ct.OUT_DB_INFO, redis_host = '127.0.0.1')
+        self.ris = RIndexStock(dbinfo = ct.OUT_DB_INFO, redis_host = '127.0.0.1')
+        self.base_color = '#e6daa6'
+        self.fig = plt.figure(facecolor = self.base_color, figsize = (24, 24))
+        self.price_ax = plt.subplot2grid((12,12), (0,0), rowspan = 6, colspan = 12, facecolor = self.base_color, fig = self.fig)
+        self.ratio_ax = plt.subplot2grid((12,12), (6,0), rowspan = 6, colspan = 12, facecolor = self.base_color, fig = self.fig)
+
+    def get_data(self, start_date, end_date):
+        uprice_df = self.ris.get_k_data_in_range(start_date, end_date)
+        lprice_df = self.rp.get_k_data_in_range(start_date, end_date)
+        iobj = CIndex('000001', dbinfo = ct.OUT_DB_INFO, redis_host = '127.0.0.1')
+        i_data = iobj.get_k_data_in_range(start_date, end_date)
+        i_data = i_data[['date', 'open', 'high', 'low', 'close', 'volume', 'amount']]
+        i_data = i_data.rename(columns = {"date": "time"})
+        i_data.time = pd.to_datetime(i_data.time)
+        i_data.time = i_data.time.apply(lambda x:date2num(x))
+        return uprice_df,lprice_df,i_data
+
+    def get_bull_ratios(self, data):
+        date_list = list()
+        rate_list = list()
+        for cdate, df in data.groupby(data.date):
+            bull_stock_num = len(df[df.profit >= 0])
+            bull_ration = 100 * bull_stock_num / len(df)
+            mdate = date2num(dt.datetime.strptime(cdate, '%Y-%m-%d'))
+            date_list.append(mdate)
+            rate_list.append(bull_ration)
+        return date_list, rate_list
+
+    def plot(self, start_date, end_date):
+        uprice_df,lprice_df,index_data = self.get_data(start_date, end_date)
+        udate_list, uratio_list = self.get_bull_ratios(uprice_df) 
+        ldate_list, lratio_list = self.get_bull_ratios(lprice_df) 
+        candlestick_ohlc(self.price_ax, index_data.values, width = 1.0, colorup = 'r', colordown = 'g')
+        self.ratio_ax.plot(udate_list, uratio_list, 'r',  label = "uprice 成本均线", linewidth = 1)
+        self.ratio_ax.plot(ldate_list, lratio_list, 'b',  label = "lprice 成本均线", linewidth = 1)
+        self.fig.autofmt_xdate()
+        plt.show()
+
+if __name__ == '__main__':
+    start_date = '2018-09-25' 
+    end_date = '2019-01-25'
+    cbr = CBullRation()
+    cbr.plot(start_date, end_date)
