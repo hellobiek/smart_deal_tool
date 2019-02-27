8e266edbe33bf31965808ad36d21c78097436861
hellobiek <hellobiek@gmail.com>
2019-02-27 10:46:28 +0800

    add new code

--- crawler/dspider/spiders/investorSituationSpider.py
+++ crawler/dspider/spiders/investorSituationSpider.py
@@ -1,8 +1,10 @@
 # -*- coding: utf-8 -*-
 import re
-import datetime
+import time
 import calendar
+import datetime
 from scrapy import Spider, FormRequest
+from datetime import datetime, timedelta
 from dspider.utils import datetime_to_str
 from dspider.items import InvestorSituationItem
 investor_count_to_path = {
@@ -26,15 +28,21 @@ class InvestorSituationSpider(Spider):
     allowed_domains = ['www.chinaclear.cn']
     start_urls = ['http://www.chinaclear.cn/cms-search/view.action']
     def start_requests(self):
-        start_date = '2019.01.20'
         formdata = dict()
         formdata['dateType'] = ''
         formdata['channelIdStr'] = '6ac54ce22db4474abc234d6edbe53ae7'
-        end_date = datetime.datetime.now().strftime('%Y.%m.%d')
+        end_date = datetime.now().strftime('%Y.%m.%d')
+        start_date = self.get_nday_ago(end_date, 30, dformat = '%Y.%m.%d')
         while start_date < end_date:
             start_date = self.get_next_date(sdate = start_date)
             formdata['dateStr'] = start_date
             yield FormRequest(url = self.start_urls[0], method = 'GET', formdata = formdata, callback = self.parse)
+
+    def get_nday_ago(self, mdate, num, dformat = "%Y%m%d"):
+        t = time.strptime(mdate, dformat)
+        y, m, d = t[0:3]
+        _date = datetime(y, m, d) - timedelta(num)
+        return _date.strftime(dformat) 
 
     def parse(self, response):
         patten = re.compile(r'[（](.*?)[）]', re.S)
@@ -50,13 +58,13 @@ class InvestorSituationSpider(Spider):
                 investor_situation_item[k] = response.xpath(investor_count_to_path[k]).extract_first().strip()
         yield investor_situation_item
 
-    def get_next_date(self, sdate = datetime.datetime.now().strftime('%Y.%m.%d'), target_day = calendar.FRIDAY):
+    def get_next_date(self, sdate = datetime.now().strftime('%Y.%m.%d'), target_day = calendar.FRIDAY):
         #func: get next date
         #sdate: str, example: '2017-01-01'
         #tdate: str, example: '2017-01-06'
         tdate = ''
-        oneday = datetime.timedelta(days = 1)
-        sdate = datetime.datetime.strptime(sdate, '%Y.%m.%d')
+        oneday = timedelta(days = 1)
+        sdate = datetime.strptime(sdate, '%Y.%m.%d')
         if sdate.weekday() == target_day: sdate += oneday
         while sdate.weekday() != target_day: 
             sdate += oneday
--- crawler/dspider/spiders/spledgeSituationSpider.py
+++ crawler/dspider/spiders/spledgeSituationSpider.py
@@ -1,7 +1,9 @@
 # -*- coding: utf-8 -*-
+import time
 import datetime
 import calendar
 from scrapy import Spider, FormRequest
+from datetime import datetime, timedelta
 from dspider.items import SPledgeSituationItem
 class SPledgeSituationSpider(Spider):
     name = 'spledgeSituationSpider'
@@ -13,11 +15,11 @@ class SPledgeSituationSpider(Spider):
     allowed_domains = ['www.chinaclear.cn']
     start_urls = ['http://www.chinaclear.cn/cms-rank/downloadFile']
     def start_requests(self):
-        start_date = '2018.03.02'
         formdata = dict()
         formdata['queryDate'] = ''
         formdata['type'] = 'proportion'
-        end_date = datetime.datetime.now().strftime('%Y.%m.%d')
+        end_date = datetime.now().strftime('%Y.%m.%d')
+        start_date = self.get_nday_ago(end_date, 30, dformat = '%Y.%m.%d')
         while start_date < end_date:
             start_date = self.get_next_date(sdate = start_date)
             formdata['queryDate'] = start_date
@@ -27,13 +29,19 @@ class SPledgeSituationSpider(Spider):
         fname = response.headers['Content-Disposition'].decode().split('=')[1]
         yield SPledgeSituationItem(file_urls = [response.url], file_name = fname)
 
-    def get_next_date(self, sdate = datetime.datetime.now().strftime('%Y.%m.%d'), target_day = calendar.FRIDAY):
+    def get_nday_ago(self, mdate, num, dformat = "%Y.%m.%d"):
+        t = time.strptime(mdate, dformat)
+        y, m, d = t[0:3]
+        _date = datetime(y, m, d) - timedelta(num)
+        return _date.strftime(dformat) 
+
+    def get_next_date(self, sdate = datetime.now().strftime('%Y.%m.%d'), target_day = calendar.FRIDAY):
         #func: get next date
         #sdate: str, example: '2017-01-01'
         #tdate: str, example: '2017-01-06'
         tdate = ''
-        oneday = datetime.timedelta(days = 1)
-        sdate = datetime.datetime.strptime(sdate, '%Y.%m.%d')
+        oneday = timedelta(days = 1)
+        sdate = datetime.strptime(sdate, '%Y.%m.%d')
         if sdate.weekday() == target_day: sdate += oneday
         while sdate.weekday() != target_day: 
             sdate += oneday
