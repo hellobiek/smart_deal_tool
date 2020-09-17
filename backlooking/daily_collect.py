97635415d656e34ad92bafbb63488557a8feb629
hellobiek <hellobiek@gmail.com>
2020-09-18 06:43:54 +0800

    add new code

--- backlooking/daily_collect.py
+++ backlooking/daily_collect.py
@@ -74,8 +74,8 @@ def generate_daily(dirname, mdate, pre_date):
         f.write(md.getStream())
 
 def main():
-    mdate = '20200911'
-    pre_date = '20200910'
+    mdate = '20200917'
+    pre_date = '20200916'
     dirname = '/Users/hellobiek/Documents/workspace/blog/blog/source/_posts'
     generate_daily(dirname, mdate, pre_date)
 
